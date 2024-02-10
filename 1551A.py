1822420988b37f8260927a00949fa92d3ac6a56d
bac0nb0yy <kyuma@pm.me>
2024-02-10 01:35:43 +0100

    problem added

--- 1551A.py
+++ 1551A.py
@@ -1,15 +1,5 @@
 for _ in range(int(input())):
     n = int(input())
-    if n == 1:
-        print(1, 0)
-    elif n == 2:
-        print(0, 1)
-    else:
-        rez = n // 3
-        if rez * 3 != n:
-            if n % rez == 1:
-                print(rez + 1, rez)
-            else:
-                print(rez, rez + 1)
-        else:
-            print(rez, rez)
+    rez = n // 3
+    mod = n % 3
+    print(rez + (mod == 1), rez + (mod == 2))
