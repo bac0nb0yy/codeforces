fb181e0da91fe4f772d6cf05b693016b9c368453
bac0nb0yy <kyuma@pm.me>
2024-02-12 19:08:29 +0100

    Hard to read problem statement but good bruteforce problem

---
+++ 0247B.py
@@ -0,0 +1,21 @@
+import itertools
+
+
+def calculate_happiness(dp, permutation):
+    happiness = 0
+    for i in range(4):
+        for k in range(i, 4, 2):
+            happiness += dp[permutation[k]][permutation[k + 1]]
+            happiness += dp[permutation[k + 1]][permutation[k]]
+    return happiness
+
+
+dp = []
+for _ in range(5):
+    dp += [list(map(int, input().split()))]
+
+best = 0
+for perm in list(itertools.permutations([0, 1, 2, 3, 4])):
+    best = max(best, calculate_happiness(dp, perm))
+
+print(best)
