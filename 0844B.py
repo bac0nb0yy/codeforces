b6d430f589912f9e9ea5d6270072fa3f9d4a62b5
bac0nb0yy <kyuma@pm.me>
2024-03-15 08:44:29 +0100

    Incroyable changes best combinatoric problem ever

--- 0844B.py
+++ 0844B.py
@@ -1,28 +1,10 @@
-def count_col(grid, x, target, rows):
-    occ = 0
-    for row in range(rows):
-        if grid[row][x] == target:
-            occ += 1
-    return occ
+import itertools
 
-
-n, m = map(int, input().split())
-
-grid = []
+h, w = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(h)]
 sets = 0
-for row in range(n):
-    grid.append(list(map(int, input().split())))
-
-for row in range(n):
-    occ_zero = grid[row].count(0)
-    occ_one = grid[row].count(1)
-    sets += (1 << occ_zero) - 1
-    sets += (1 << occ_one) - 1
-
-for col in range(m):
-    occ_zero = count_col(grid, col, 0, n)
-    occ_one = count_col(grid, col, 1, n)
-    sets += (1 << occ_zero) - 1
-    sets += (1 << occ_one) - 1
-
-print(sets - n * m)
+for line in itertools.chain(grid, zip(*grid)):
+    occ_zero = line.count(0)
+    occ_one = len(line) - occ_zero
+    sets += (1 << occ_zero) + (1 << occ_one)
+print(sets - h * w - 2 * (h + w))
