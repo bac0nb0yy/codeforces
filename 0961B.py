6ba1195ee7499f7e29cc1de7251661d6e3d8eba8
bac0nb0yy <kyuma@pm.me>
2024-03-15 08:44:42 +0100

    Super changes on the best DP problem

--- 0961B.py
+++ 0961B.py
@@ -2,24 +2,9 @@ n, k = map(int, input().split())
 a = list(map(int, input().split()))
 t = list(map(int, input().split()))
 
-dp_yes = [0] * (n)
-dp_no = [0] * (n)
-
-for i in range(n):
-    dp_yes[i] = dp_yes[i - 1] + a[i]
-    dp_no[i] = dp_no[i - 1] + a[i] * t[i]
-
-maxi = dp_no[-1]
-
-for i in range(n - k + 1):
-    maxi = max(
-        maxi,
-        (
-            dp_no[i - 1] + (dp_yes[i + k - 1] - dp_yes[i - 1])
-            if i > 0
-            else dp_yes[i + k - 1]
-        )
-        + (dp_no[-1] - dp_no[i + k - 1]),
-    )
-
-print(maxi)
+start = sum(map(int.__mul__, a, t))
+rez = current = start + sum(ai for ai, ti in zip(a[:k], t[:k]) if not ti)
+for i in range(k, n):
+    current += (1 - t[i]) * a[i] - (1 - t[i - k]) * a[i - k]
+    rez = max(current, rez)
+print(rez)
