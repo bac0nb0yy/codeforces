999f802330423f8883cdcc51f807b4f8e517508c
bac0nb0yy <kyuma@pm.me>
2024-03-15 06:43:23 +0100

    Number theory problem cool added some notes, and optimized it

--- 0742A.py
+++ 0742A.py
@@ -1,24 +1,15 @@
+import math
+
 n = int(input())
-
-dp = [0] * 10
-dp[0] = 1
-dp[1] = 8
-dp[2] = 4
-dp[3] = 2
-dp[4] = 6
-dp[5] = 8
-dp[6] = 4
-dp[7] = 2
-dp[8] = 6
-dp[9] = 8
-
 
 # on peut décomposer k^n en : k^y * k^x * k^z...
 # tant que x + y + z + ... = n
+# (8^10) % 10 = 4
+# (8^(10^k)) % 10 = 6 avec k > 1
 
 rez = 1
 while n >= 10:
-    rez = (rez * 4) % 10
-    n -= 10
+    rez = (rez * (4 + 2 * (n >= 100))) % 10
+    n -= 10 ** int(math.log(n, 10))
 
-print((rez * dp[n]) % 10)
+print(rez * (8**n) % 10)
